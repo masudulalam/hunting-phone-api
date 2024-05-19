7b80f37b30d5f9dae9bfb6fb021b293aa3e3af05
masudulalam <masuudulalam@gmail.com>
2024-05-20 00:17:56 +0600

    loaded data with async await function

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,7 @@
-console.log('inside phone.js');
+const loadPhone = async () => {
+    const res = await fetch ('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json();
+    console.log(data.data);
+}
+
+loadPhone();
